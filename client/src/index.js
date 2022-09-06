37e7ecb3bf40c527040d58a50bb48705c06df4ff
Johnathan Reese <JohnathanReese8@gmail.com>
2022-09-06 16:33:41 -0400

    Cleaned up packages

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     //removed <React.StrictMode> because it was causing errors i.e. TypeError: Cannot read properties of undefined (reading 'dispose')
     <App />
 );
-
-serviceWorker.register();
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cluster0', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pet-intelligent', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
