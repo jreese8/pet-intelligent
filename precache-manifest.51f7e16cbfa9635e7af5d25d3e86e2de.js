b28d268b192587ce51ced43113ecaf1da73b7f16
Johnathan Reese <JohnathanReese8@gmail.com>
2022-09-06 14:34:34 -0400

    Updates

---
+++ precache-manifest.51f7e16cbfa9635e7af5d25d3e86e2de.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "189129b59b0f7f2a05da",
+    "url": "/static/css/main.089ad4d8.chunk.css"
+  },
+  {
+    "revision": "189129b59b0f7f2a05da",
+    "url": "/static/js/main.189129b5.chunk.js"
+  },
+  {
+    "revision": "925dda96d98ac89208c1",
+    "url": "/static/js/1.925dda96.chunk.js"
+  },
+  {
+    "revision": "c2b8d03445c6e0e12e12",
+    "url": "/static/css/2.f84cbdfa.chunk.css"
+  },
+  {
+    "revision": "c2b8d03445c6e0e12e12",
+    "url": "/static/js/2.c2b8d034.chunk.js"
+  },
+  {
+    "revision": "11574f9d1304829ae0a4",
+    "url": "/static/js/runtime~main.11574f9d.js"
+  },
+  {
+    "revision": "1af68cbeb63f40d4262f43779e113161",
+    "url": "/static/media/loading.1af68cbe.gif"
+  },
+  {
+    "revision": "a35989f960a6ccf5f74c18be9b133dfd",
+    "url": "/static/media/logo.a35989f9.png"
+  },
+  {
+    "revision": "b7593f4142afcb8c5897e2f9083d8cee",
+    "url": "/static/media/dogncat.b7593f41.png"
+  },
+  {
+    "revision": "14e9c49219b96c44fe6ebae7f95f1739",
+    "url": "/static/media/dogncat2.14e9c492.jpg"
+  },
+  {
+    "revision": "72ce0fe771db2723218c48cd04e57e80",
+    "url": "/static/media/dogncat3.72ce0fe7.png"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "f86d9bf34b5da8610c6a218e96667b68",
+    "url": "/static/media/react-bootstrap-carousel.f86d9bf3.svg"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "ef7b846356f6b009184e201b2f69bd98",
+    "url": "/index.html"
+  }
+];
