a9d92e51403474893c41f2d946083d4ba70c364e
Johnathan Reese <JohnathanReese8@gmail.com>
2023-01-11 17:33:37 -0500

    localhost

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/pet-intelligent', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pet-intelligent', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
